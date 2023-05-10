d6738d173c4a173f4cc273b23f773ea57a618c30
Angle-Zz <130260417+Angle-Zz@users.noreply.github.com>
2023-05-10 09:41:57 +0800

    Update settings.py

--- Django/untitled/settings.py
+++ Django/untitled/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+ALLOWED_HOSTS = ['anglezz.pythonanywhere.com']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
